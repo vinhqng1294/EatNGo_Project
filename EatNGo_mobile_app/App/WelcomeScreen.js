b3155ad84d531ddf8d1f8d74b629c7fcdfc2b50a
vinhqng1294 <quangvinh.nguyen94@outlook.com>
2019-03-04 22:11:32 +0700

    done welcome screen

---
+++ EatNGo_mobile_app/App/WelcomeScreen.js
@@ -0,0 +1,98 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+} from 'react-native';
+
+type Props = {};
+export default class WelcomeScreen extends Component<Props> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.logoTitleContainer}>
+                    <Image source={require('../Assets/eatngo_logo_trans.png')}
+                        style={styles.logoImg} />
+                </View>
+
+                <View style={styles.loginOptionContainer}>
+                    <Text style={styles.normalText}>
+                        Sign in using
+                    </Text>
+                    <TouchableOpacity style={styles.option}>
+                        <Image source={require('../Assets/facebook.png')}
+                            style={styles.img} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.option}>
+                        <Image source={require('../Assets/google.png')}
+                            style={styles.img} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.option}>
+                        <Image source={require('../Assets/mobile.png')}
+                            style={styles.img} />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.footer}>
+                </View>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+        backgroundColor: '#EBEBEB',
+    },
+    logoTitleContainer: {
+        flex: 3,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#EBEBEB',
+    },
+    loginOptionContainer: {
+        flex: 3,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#EBEBEB',
+        alignItems: 'center',
+    },
+    logoImg: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'contain',
+    },
+    normalText: {
+        textAlign: 'center',
+        fontSize: 23,
+        color: '#878787',
+        fontFamily: ''
+    },
+    option: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#EBEBEB',
+        width: 110,
+    },
+    img: {
+        width: 50,
+        height: 50,
+        resizeMode: 'contain',
+    },
+    footer: {
+        flex: .5,
+        backgroundColor: '#EBEBEB',
+    },
+});
