53b4a3a8c525ee310e61a293cd643d549dcb7f7e
vinhqng1294 <quangvinh.nguyen94@outlook.com>
2019-03-06 18:03:07 +0700

    finish welcome screen

--- EatNGo_mobile_app/App/WelcomeScreen.js
+++ EatNGo_mobile_app/App/WelcomeScreen.js
@@ -15,7 +15,7 @@ export default class WelcomeScreen extends Component<Props> {
         return (
             <View style={styles.container}>
                 <View style={styles.logoTitleContainer}>
-                    <Image source={require('../Assets/eatngo_logo_trans.png')}
+                    <Image source={require('../assets/eatngo_logo_trans.png')}
                         style={styles.logoImg} />
                 </View>
 
@@ -25,17 +25,17 @@ export default class WelcomeScreen extends Component<Props> {
                     </Text>
                     <TouchableOpacity style={styles.option}>
                         {/* onPress = () => {}; */}
-                        <Image source={require('../Assets/facebook.png')}
+                        <Image source={require('../assets/facebook.png')}
                             style={styles.img} />
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.option}>
                         {/* onPress = () => {}; */}
-                        <Image source={require('../Assets/google.png')}
+                        <Image source={require('../assets/google.png')}
                             style={styles.img} />
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.option}>
                         {/* onPress = () => {}; */}
-                        <Image source={require('../Assets/mobile.png')}
+                        <Image source={require('../assets/mobile.png')}
                             style={styles.img} />
                     </TouchableOpacity>
                 </View>
@@ -77,8 +77,9 @@ const styles = StyleSheet.create({
     },
     normalText: {
         textAlign: 'center',
-        fontSize: 23,
+        fontSize: 18,
         color: '#878787',
+        fontFamily: 'Quicksand',
     },
     option: {
         flex: 1,
